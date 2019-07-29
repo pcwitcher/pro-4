eb3027e20fb8c9f83cb1975b1fcb3bd19bec2091
PCWitcher <pcwitcher.ie@gmail.com>
2019-07-29 21:28:38 +0100

    work with js toggle

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const burger = document.querySelector(".burger");
+const nav = document.querySelector(".ul-list");
 
 const iconBurger = document.querySelector(".fa-bars");
 const iconX = document.querySelector(".fa-times");
-const menu = document.querySelector(".menu");
+const menu = document.querySelector(".ul-list");
 
 
 burger.addEventListener("click", function () {
     iconBurger.classList.toggle("show"); //tak
     iconX.classList.toggle("show"); //nie
-    menu.classList.toggle("show"); //nie
+})
 
+nav.addEventListener("click", function () {
+    menu.classList.toggle(".pokaz"); //nie
 })
