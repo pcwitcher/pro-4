70bc0b504a3f91e9ab9d1f21dc98d2dd65ca8eec
PCWitcher <pcwitcher.ie@gmail.com>
2019-07-30 18:03:43 +0100

    responsive menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 const burger = document.querySelector(".burger");
-const nav = document.querySelector(".ul-list");
 
 const iconBurger = document.querySelector(".fa-bars");
 const iconX = document.querySelector(".fa-times");
-const menu = document.querySelector(".ul-list");
+const menu = document.querySelector(".menu");
 
 
 burger.addEventListener("click", function () {
     iconBurger.classList.toggle("show"); //tak
-    iconX.classList.toggle("show"); //nie
-})
+    iconX.classList.toggle("show");
+    menu.classList.toggle("show"); //nie
+    //burger.classList.toggle('show')
+});
 
-nav.addEventListener("click", function () {
-    menu.classList.toggle(".pokaz"); //nie
-})
+/*burger.addEventListener("click", function () {
+    this.classList.toggle("show");
+    menu.classList.toggle("pokaz");
+})*/
+
+/*const menu = document.getElementById('menu');
+const nav = document.getElementById('nav');
+const exit = document.getElementById('exit');
+
+menu.addEventListener('click', function (e) {
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault();
+});
+
+exit.addEventListener('click', function (e) {
+    nav.classList.add('hide-mobile');
+    e.preventDefault();
+});*/
